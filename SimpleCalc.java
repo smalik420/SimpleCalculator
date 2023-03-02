a4a4f362f9f17dbb759eb75b1149be3b7c085b9b
smalik420 <122914028+smalik420@users.noreply.github.com>
2023-03-02 09:59:46 -0800

    Update SimpleCalc.java

--- SimpleCalc.java
+++ SimpleCalc.java
@@ -49,55 +49,28 @@ public class SimpleCalc {
 	public void runCalc() 
 	{
 		String userInput = Prompt.getString("\n");
-		userInput = utils.cleanExpr(userInput);
-		if (userInput.equalsIgnoreCase("h")			printHelp();
-		else if (userInput.equalsIgnoreCase("q")	codeRunning = false;
-		
-		List<String> expressionTokens = utils.tokenizeExpression();
-		
-		while (valueStack.size() != 1)
-		{
-			int pos = 0;
-			int currentStr = expressionTokens.get(i);
-			while (pos < expressionTokens.size - 1)
-			{
-				if (currentStr.isDigit)
-				{
-					valueStack.push(currentStr);
-				}
-				else if (utils.isOperator(currentStr))
-				{
-					if (operatorStack.isEmpty)
-					{
-						operatorStack.push(currentStr);
-					}
-					else
-					{
-						String last = operatorStack.peek();
-						boolean shudPop = hasPrecedence(last, currentStr);
-						
-						
-						
-						
-					}
-					
-					
-					
-					
-					
-					
-				}
-				pos++;
-			} 
-			
-			
+		if (userInput.equalsIgnoreCase("h") )			printHelp();
+		else if (userInput.equalsIgnoreCase("q") )	codeRunning = false;
+		else
+		{	
+			List<String> expressionTokens = utils.tokenizeExpression(userInput);
+			double answer = evaluateExpression(expressionTokens);
+			System.out.println(answer);
 		}
-		
-		
-		
-		
-		return valueStack.get(0);
+					
 	}
+	
+	public boolean isStringDigit(String str)
+	{
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (Character.isDigit(str.charAt(i)) == false)
+				return false;
+		}
+		return true;
+	}
+	
+	
 	
 	/**	Print help */
 	public void printHelp() {
@@ -114,11 +87,93 @@ public class SimpleCalc {
 	 *	@param tokens	a List of String tokens making up an arithmetic expression
 	 *	@return			a double value of the evaluated expression
 	 */
-	public double evaluateExpression(List<String> tokens) {
+	public double evaluateExpression(List<String> tokens) 
+	{
 		double value = 0;
 		
+		int tPos = 0;
+		for (int i = 0; i < tokens.size(); i++)
+		{
+			
+			
+		}
+		
+		
+		
+		while ( !operatorStack.isEmpty() )
+		{
+			String current = tokens.get(tPos);
+			
+			if (isStringDigit(current) )
+			{
+				valueStack.push(Double.parseDouble(current));
+				tPos++;
+			}
+			else
+			{
+				if (operatorStack.isEmpty() )
+				{
+					operatorStack.push(current);
+					tPos++;
+				}
+				else
+				{
+					//if true, then peek has higher or same precedence
+					boolean peekPrecedence = hasPrecedence(current, operatorStack.peek());
+					
+					if (peekPrecedence)
+					{
+						double double1 = valueStack.pop();
+						double double2 = valueStack.pop();
+						String operatorStr = operatorStack.peek();
+						operatorStack.pop();
+						double answer = runMathEquation(double1, current, double2);
+						System.out.println(answer);
+						value += answer;
+						
+						tPos++;
+					}
+					
+					operatorStack.push(current);
+					tPos++;
+					
+					
+				}
+			}
+		}
+
 		return value;
 	}
+	
+//	for (int i = 0; i < expressionTokens.size(); i++) {
+//				System.out.println(expressionTokens.get(i));
+//	}
+	
+	public double runMathEquation(double val1, String operate, double val2)
+	{
+		if (operate.equals("+") )
+		{
+			return val1 + val2;
+		}
+		else if (operate.equals("-") )
+		{
+			return val1 - val2;
+		}
+		else if (operate.equals("*") )
+		{
+			return val1 * val2;
+		}
+		else if (operate.equals("/") )
+		{
+			return val1 / val2;
+		}
+		else
+		{
+			System.out.println("ERROR OCCURED check runMathEquation");
+			return 0;
+		}
+	}
+	
 	
 	/**
 	 *	Precedence of operators
